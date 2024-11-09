767e30f3ebfdcd638875d4d7bfb080dfd6dc116c
Akash632 <akashnagineni632@gmail.com>
2024-11-09 16:38:45 +0530

    final commit

--- LargestElementInAnArray.java
+++ LargestElementInAnArray.java
@@ -1,6 +1,39 @@
 
+// Given an array arr[]. The task is to find the largest element and return it.
+
+// Input: 3
+// 20
+// 90
+// -10
+// Output: 90
+// Explanation: The largest element of the given array is 90.
+
+
+import java.util.*;
+
 public class LargestElementInAnArray{
+
+    static int FindLargestElement(int[] arr, int size){
+        int max = Integer.MIN_VALUE;
+
+        for(int i=0; i<size; i++){
+            if(arr[i] > max){
+                max = arr[i];
+            }
+        }
+        return max;
+    }
     public static void main(String[] args){
-        System.out.println("Hello");
+        Scanner sc = new Scanner(System.in);
+
+        int size = sc.nextInt();
+        int[] arr = new int[size];
+
+        for(int i=0; i<size; i++){
+            arr[i] = sc.nextInt(); sc.nextLine();
+        }
+
+        int result = FindLargestElement(arr, size);
+        System.out.println(result);
     }
 }
